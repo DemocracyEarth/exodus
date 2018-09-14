02df0bce102ea6cc149bb1d4470501ec9c115ccd
medied <medina.eag@gmail.com>
2018-09-14 12:33:17 -0400

    config

--- truffle-config.js
+++ truffle-config.js
@@ -2,13 +2,26 @@
 require('babel-register');
 require('babel-polyfill');
 
+/* README - debug
+* have played around with `gas` and `gasPrice`,
+* adding the `solc`, all attempts to solve gas 
+* issues.  
+*/
+
 module.exports = {
   networks: {
     local: {
       host: 'localhost',
       port: 9545,
-      gas: 5000000,
+      gas: 5,
+      gasPrice: 10000000000000000000000,
       network_id: '*'
+    }
+  },
+  solc: {
+    optimizer: {
+      enabled: true,
+      runs: 200
     }
   }
 };
