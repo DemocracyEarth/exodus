9cd73e125eca1e20c22f678ad603193e403754ef
medied <medina.eag@gmail.com>
2018-11-12 16:50:14 -0500

    approach-B

--- contracts/token/Vote.sol
+++ contracts/token/Vote.sol
@@ -1,20 +1,11 @@
 pragma solidity ^0.4.24;
+import "zos-lib/contracts/Initializable.sol";
 import "openzeppelin-eth/contracts/token/ERC20/StandaloneERC20.sol";
 
-contract Vote is StandaloneERC20 {
-    string public _name;
-    string public _symbol;
-    uint8 public _decimals;
-    uint256 public _INITIAL_SUPPLY;
-    address[] _minters;
-    address[] _pausers;
-
-    function initialize(address initialAccount) public initializer {
-        _name = "Democracy Earth Vote Token";
-        _symbol = "VOTE";
-        _decimals = 18;
-        _INITIAL_SUPPLY = 100000000 * 10**uint256(_decimals);
-
-        StandaloneERC20.initialize(_name, _symbol, _decimals, _INITIAL_SUPPLY, initialAccount, _minters, _pausers);
+contract Vote is Initializable {
+    StandaloneERC20 public token;
+    
+    function initialize(StandaloneERC20 _token) public initializer {
+        token = _token;
     }
 }
