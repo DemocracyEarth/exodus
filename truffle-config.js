fad9f3c28c92465e550303a96efc3e242900d865
medied <medina.eag@gmail.com>
2018-11-14 18:24:42 -0500

    update config

--- truffle-config.js
+++ truffle-config.js
@@ -3,13 +3,7 @@ require('babel-register');
 require('babel-polyfill');
 var HDWalletProvider = require("truffle-hdwallet-provider");
 
-/* README - debug
-* have played around with `gas` and `gasPrice`,
-* adding the `solc`, all attempts to solve gas 
-* issues.  
-*/
-
-var mnemonic = "glad cereal slogan limb salon spin father bulb poem paper siren robot";
+var mnemonic;
 
 module.exports = {
   networks: {
@@ -23,6 +17,20 @@ module.exports = {
         return new HDWalletProvider(mnemonic, "https://rinkeby.infura.io/v3/b1825422de3d471eba20968cf22b85f1")
       },
       network_id: 4
+    },
+    ropsten: {
+      provider: () => {
+        return new HDWalletProvider(mnemonic, "https://mainnet.infura.io/v3/1158c2468d8f4826b05ff6997368fdd4")
+      },
+      network_id: 3,
+      gasPrice: 1000000000,
+    },
+    kovan: {
+      provider: function() {
+        return new HDWalletProvider(mnemonic, "https://kovan.infura.io/v3/b1825422de3d471eba20968cf22b85f1")
+      },
+      network_id: 42,
+      gasPrice: 2,
     }
   },
 };
